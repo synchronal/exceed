e4069fb0d6aa81ad22d9ae68b46ceea8cb1ddd00
Eric Saxby <sax@livinginthepast.org>
2025-01-19 10:55:40 -0800

    Benchmarks skip actual file generation

--- benchmark/bench.exs
+++ benchmark/bench.exs
@@ -2,29 +2,29 @@ defmodule Benchmark do
   require Logger
 
   def buffered(column_count \\ 10, row_count \\ 100_000) do
-    file = File.stream!("/tmp/workbook.xlsx")
     headers = headers(column_count)
     stream = stream(column_count, row_count)
 
     benchmark(column_count, row_count, fn ->
-      Exceed.Workbook.new("Creator Name")
-      |> Exceed.Workbook.add_worksheet(Exceed.Worksheet.new("Sheet Name", headers, stream))
-      |> Exceed.stream!()
-      |> Stream.into(file)
+      Exceed.Worksheet.new("Sheet Name", headers, stream)
+      |> Exceed.Worksheet.to_xml()
+      |> Exceed.File.file("xl/worksheets/sheet1.xml", [])
+      |> List.wrap()
+      |> Zstream.zip()
       |> Stream.run()
     end)
   end
 
   def unbuffered(column_count \\ 10, row_count \\ 100_000) do
-    file = File.stream!("/tmp/workbook.xlsx")
     headers = headers(column_count)
     stream = stream(column_count, row_count)
 
     benchmark(column_count, row_count, fn ->
-      Exceed.Workbook.new("Creator Name")
-      |> Exceed.Workbook.add_worksheet(Exceed.Worksheet.new("Sheet Name", headers, stream))
-      |> Exceed.stream!(buffer: false)
-      |> Stream.into(file)
+      Exceed.Worksheet.new("Sheet Name", headers, stream)
+      |> Exceed.Worksheet.to_xml()
+      |> Exceed.File.file("xl/worksheets/sheet1.xml", buffer: false)
+      |> List.wrap()
+      |> Zstream.zip()
       |> Stream.run()
     end)
   end
