fd67c416c1919abfc7f1355e611b2a0988daa441
Eric Saxby <sax@reflective.dev>
2026-04-29 12:31:02 -0700

    Use xlsx_reader (in tests) from hex

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Exceed.MixProject do
       {:ex_doc, "~> 0.31", only: [:docs, :dev], runtime: false},
       {:mix_audit, "~> 2.0", only: :dev, runtime: false},
       {:moar, "~> 4.0", only: :test},
-      {:xlsx_reader, "~> 0.8", only: :test, github: "xavier/xlsx_reader"},
+      {:xlsx_reader, "~> 0.8", only: :test},
       {:xml_query, "> 0.0.0", only: :test},
       {:xml_stream, "~> 0.3"},
       {:zstream, "~> 0.6.4"}
